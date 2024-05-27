28aa6d30f2a0e69ac6b1daa4458658a287c5128c
Fredy <fredzuna@hotmail.com>
2024-05-27 00:03:36 -0400

    controller and service unit test

--- src/favorite-product/favorite-product.controller.spec.ts
+++ src/favorite-product/favorite-product.controller.spec.ts
@@ -1,22 +1,124 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FavoriteProductController } from './favorite-product.controller';
 import { FavoriteProductService } from './favorite-product.service';
+import { CreateFavoriteProductDto } from './dto/create-favorite-product.dto';
+import { FavoriteProduct } from './entities/favorite-product.entity';
 
 describe('FavoriteProductController', () => {
   let controller: FavoriteProductController;
+  let service: FavoriteProductService;
+
+  const mockFavoriteProductService = {
+    create: jest.fn((dto: CreateFavoriteProductDto) => {
+      return Promise.resolve({
+        id: Date.now(),
+        ...dto,
+      } as FavoriteProduct);
+    }),
+    findAll: jest.fn(() =>
+      Promise.resolve([
+        {
+          id: 1,
+          productId: 'product1',
+          name: 'favorite Product',
+          description: 'Test Description',
+          price: 100,
+          imageUrl: 'http://images.com/image.jpg',
+          jsonData: '{"key":"value"}',
+        },
+      ]),
+    ),
+    findOneByProductId: jest.fn((productId: string) => {
+      return Promise.resolve({
+        id: 1,
+        productId,
+        name: 'favorite Product',
+        description: 'Test Description',
+        price: 100,
+        imageUrl: 'http://images.com/image.jpg',
+        jsonData: '{"key":"value"}',
+      } as FavoriteProduct);
+    }),
+    removeByProductId: jest.fn((productId: string) => Promise.resolve()),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FavoriteProductController],
-      providers: [FavoriteProductService],
+      providers: [
+        {
+          provide: FavoriteProductService,
+          useValue: mockFavoriteProductService,
+        },
+      ],
     }).compile();
 
     controller = module.get<FavoriteProductController>(
       FavoriteProductController,
     );
+    service = module.get<FavoriteProductService>(FavoriteProductService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a favorite product', async () => {
+      const dto: CreateFavoriteProductDto = {
+        productId: 'product1',
+        name: 'favorite Product',
+        description: 'Test Description',
+        price: 100,
+        imageUrl: 'http://images.com/image.jpg',
+        jsonData: '{"key":"value"}',
+      };
+      expect(await controller.create(dto)).toEqual({
+        id: expect.any(Number),
+        ...dto,
+      });
+      expect(service.create).toHaveBeenCalledWith(dto);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of favorite products', async () => {
+      expect(await controller.findAll()).toEqual([
+        {
+          id: 1,
+          productId: 'product1',
+          name: 'favorite Product',
+          description: 'Test Description',
+          price: 100,
+          imageUrl: 'http://images.com/image.jpg',
+          jsonData: '{"key":"value"}',
+        },
+      ]);
+      expect(service.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOneByProductId', () => {
+    it('should return a single favorite product by productId', async () => {
+      const productId = 'product1';
+      expect(await controller.findOneByProductId(productId)).toEqual({
+        id: 1,
+        productId,
+        name: 'favorite Product',
+        description: 'Test Description',
+        price: 100,
+        imageUrl: 'http://images.com/image.jpg',
+        jsonData: '{"key":"value"}',
+      });
+      expect(service.findOneByProductId).toHaveBeenCalledWith(productId);
+    });
+  });
+
+  describe('removeByProductId', () => {
+    it('should remove a favorite product by productId', async () => {
+      const productId = 'product1';
+      await controller.removeByProductId(productId);
+      expect(service.removeByProductId).toHaveBeenCalledWith(productId);
+    });
   });
 });
--- src/favorite-product/favorite-product.service.spec.ts
+++ src/favorite-product/favorite-product.service.spec.ts
@@ -1,18 +1,125 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FavoriteProductService } from './favorite-product.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { FavoriteProduct } from './entities/favorite-product.entity';
+import { CreateFavoriteProductDto } from './dto/create-favorite-product.dto';
+import { InternalServerErrorException } from '@nestjs/common';
 
 describe('FavoriteProductService', () => {
   let service: FavoriteProductService;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let repository: Repository<FavoriteProduct>;
+
+  const mockFavoriteProductRepository = {
+    save: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    delete: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FavoriteProductService],
+      providers: [
+        FavoriteProductService,
+        {
+          provide: getRepositoryToken(FavoriteProduct),
+          useValue: mockFavoriteProductRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<FavoriteProductService>(FavoriteProductService);
+    repository = module.get<Repository<FavoriteProduct>>(
+      getRepositoryToken(FavoriteProduct),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a favorite product', async () => {
+      const createDto: CreateFavoriteProductDto = {
+        productId: 'product-1',
+        name: 'Test favorite product',
+        description: 'favorite description',
+        price: 100,
+        imageUrl: 'http://images.com/image111.jpg',
+        jsonData: '{"key":"value"}',
+      };
+      const favoriteProduct = { id: 1, ...createDto };
+      mockFavoriteProductRepository.save.mockResolvedValue(favoriteProduct);
+
+      expect(await service.create(createDto)).toEqual(favoriteProduct);
+      expect(mockFavoriteProductRepository.save).toHaveBeenCalledWith(
+        createDto,
+      );
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of favorite products', async () => {
+      const favoriteProducts = [
+        {
+          id: 1,
+          productId: 'product-1',
+          name: 'Test favorite product',
+          description: 'favorite description',
+          price: 100,
+          imageUrl: 'http://images.com/image111.jpg',
+          jsonData: '{"key":"value"}',
+        },
+      ];
+      mockFavoriteProductRepository.find.mockResolvedValue(favoriteProducts);
+
+      expect(await service.findAll()).toEqual(favoriteProducts);
+      expect(mockFavoriteProductRepository.find).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOneByProductId', () => {
+    it('should return a single favorite product by productId', async () => {
+      const productId = 'product-1';
+      const favoriteProduct = {
+        id: 1,
+        productId,
+        name: 'Test favorite product',
+        description: 'favorite description',
+        price: 100,
+        imageUrl: 'http://images.com/image111.jpg',
+        jsonData: '{"key":"value"}',
+      };
+      mockFavoriteProductRepository.findOne.mockResolvedValue(favoriteProduct);
+
+      expect(await service.findOneByProductId(productId)).toEqual(
+        favoriteProduct,
+      );
+      expect(mockFavoriteProductRepository.findOne).toHaveBeenCalledWith({
+        where: { productId },
+      });
+    });
+
+    it('should throw an InternalServerErrorException on error', async () => {
+      const productId = 'product-1';
+      mockFavoriteProductRepository.findOne.mockRejectedValue(new Error());
+
+      await expect(service.findOneByProductId(productId)).rejects.toThrow(
+        InternalServerErrorException,
+      );
+    });
+  });
+
+  describe('removeByProductId', () => {
+    it('should remove a favorite product by productId', async () => {
+      const productId = 'product-1';
+      mockFavoriteProductRepository.delete.mockResolvedValue({});
+
+      await service.removeByProductId(productId);
+      expect(mockFavoriteProductRepository.delete).toHaveBeenCalledWith({
+        productId,
+      });
+    });
   });
 });
