8ccf0e83f24181eee679876f1806b0663ccbf156
Fredy <fredzuna@hotmail.com>
2024-05-26 23:24:31 -0400

    configure cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api'); // Set the global prefix to '/api'
+  app.useGlobalPipes(new ValidationPipe()); // Enable global validation
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // URL of the frontend application
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
+  await app.listen(5000);
 }
 bootstrap();
